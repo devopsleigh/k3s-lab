6c71f508482098bf11a471d9ebd584be6328e2ce
Leigh <31065424+devopsleigh@users.noreply.github.com>
2020-02-17 14:15:24 +1000

    Renamed the cluster

--- copy-ssh-keys.sh
+++ copy-ssh-keys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 test ! -f ~/.ssh/id_rsa.pub && ssh-keygen -t rsa
-for host in `ansible k3s-cluster --list-hosts | grep k3s- | sed -e 's/^[ \t]*//'`; do
-    ssh-copy-id -i ~/.ssh/id_rsa.pub pi@$host
+for host in `ansible cluster --list-hosts | grep k3s- | sed -e 's/^[ \t]*//'`; do
+    ssh-copy-id -i ~/.ssh/id_rsa.pub pi@$host -f
 done
